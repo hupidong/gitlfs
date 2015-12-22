7eebfd55951b614bc765651452111fcb0fd90825
Ananda <anandan@umich.edu>
2015-12-22 16:50:54 -0500

    add to extract script

--- dstc1/extract.sh
+++ dstc1/extract.sh
@@ -1,4 +1,14 @@
 . ~/.bashrc
+
 extract dstc_scripts.tgz
 extract dstc_data_train3_v00.tgz
 extract dstc_data_test3.tgz
+
+extract train1c.tar.gz
+extract train1a.tar.gz
+extract train1b.tar.gz
+extract test2.tar.gz
+extract test1.tar.gz
+extract train2.tar.gz
+extract test4.tar.gz
+extract letsgo_db_201004.tar.gz
